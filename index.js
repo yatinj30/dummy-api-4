0e9e9b8fe5730e8c3975f1b50c376a2876586779
jaykukadiya99 <jaykukadiya99@gmail.com>
2023-12-14 11:41:00 +0530

    fix: remove unused code

--- index.js
+++ index.js
@@ -4,15 +4,9 @@ const fileData = require('./test.json');
 
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
-
-    // While there remain elements to shuffle.
     while (currentIndex > 0) {
-
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
-
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex], array[currentIndex]];
     }
@@ -21,21 +15,12 @@ function shuffle(array) {
 }
 
 async function main() {
-    // console.log(fileData.mainScreenContent.mainListView);
-
-    // const newJson = 
-    // await fs.writeFile("test2.json", readme);
-
     fileData.mainScreenContent.mainListView.forEach((item) => {
         if (item.isShuffle) {
             let data = item.songs
             shuffle(data);
-            console.log("-------");
-            console.log(JSON.stringify(data));
         }
     });
-
-    // const newJson = 
     await fs.writeFile("test.json", JSON.stringify(fileData));
 }
 
